465272cc46fa4689f1c00dcff43c9fa0459522eb
Andras Varga <vargan89@gmail.com>
2026-05-07 12:10:53 +0800

    Fix Vercel config - use app.js for API

--- api/events.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-  res.json({ message: 'Events endpoint' });
-});
-
-module.exports = router;
